c5146986b42a5de2f3b7cf6f933b2ed2d2a7af47
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-15 22:22:20 +0200

    added info.js script to print date, time, and system info

---
+++ info.js
@@ -0,0 +1,5 @@
+console.log("👋 Hello, Node.js!");
+console.log("📅 Current Date:", new Date().toLocaleDateString());
+console.log("🕒 Current Time:", new Date().toLocaleTimeString());
+console.log("🖥️ Platform:", process.platform);
+console.log("📂 Current Directory:", __dirname);
