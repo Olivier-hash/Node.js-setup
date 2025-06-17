1e682eec8d4cffb9f0facd107d5477c4fc0c61f1
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-17 22:15:25 +0200

    Added env.js to display Node.js runtime environment info

---
+++ env.js
@@ -0,0 +1,5 @@
+// env.js
+console.log("🌍 Environment Information");
+console.log("Node Version:", process.version);
+console.log("Platform:", process.platform);
+console.log("Architecture:", process.arch);
